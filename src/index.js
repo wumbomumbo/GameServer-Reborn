a6760dd12dcd0f07b4b5f6f76fa157b9f6ee3a00
Glitched Panda <82221876+GlitchedPanda@users.noreply.github.com>
2025-04-05 08:00:25 +0200

    Quickfix

--- src/index.js
+++ src/index.js
@@ -73,8 +73,7 @@ app.use((req, res, next) => {
 app.use(
   compression({
     filter: () => true,
-    threshold: 0,
-    level: 6, // Good balance between compression ratio and CPU usage
+    threshold: 0
   }),
 );
 
